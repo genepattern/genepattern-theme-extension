aa10d52b4dedb5961a7742bbc452b9500708c2e5
tmtabor <tmtabor@cloud.ucsd.edu>
2020-12-10 09:40:03 -0800

    Updating GenePatter theme for JupyterLab 2.x

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * A GenePattern theme for JupyterLab
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
